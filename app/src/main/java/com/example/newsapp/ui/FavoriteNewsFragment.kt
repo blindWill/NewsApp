d6436c964bff5a2c494d1291d5835163578a3351
blindWill <andrey.grabnyuk@mail.ru>
2023-01-09 18:02:36 +0300

    Create base layout

---
+++ app/src/main/java/com/example/newsapp/ui/FavoriteNewsFragment.kt
@@ -0,0 +1,21 @@
+package com.example.newsapp.ui
+
+import androidx.fragment.app.Fragment
+import com.example.newsapp.R
+
+
+class FavoriteNewsFragment : Fragment(R.layout.fragment_latest_news) {
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//    }
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_favorite_news, container, false)
+//    }
+
+}
---
+++ app/src/main/java/com/example/newsapp/ui/LatestNewsFragment.kt
@@ -0,0 +1,24 @@
+package com.example.newsapp.ui
+
+import androidx.fragment.app.Fragment
+import com.example.newsapp.R
+
+class LatestNewsFragment : Fragment(R.layout.fragment_favorite_news) {
+
+
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//    }
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        return inflater.inflate(R.layout.fragment_current_news, container, false)
+//    }
+
+
+
+}
